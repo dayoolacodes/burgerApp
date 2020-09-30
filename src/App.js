528d6d2e33fa0dd218273e72b1fbd250f5463090
dayoola <ayodayo1@gmail.com>
2020-09-30 14:05:39 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Layout from "./hoc/Layout/Layout"
 
 
 function App() {
-  const [show, setShow] = useState(true)
+  // const [show, setShow] = useState(true)
+  // const [timer, setTimer] = useState(0)
 
-  setTimeout(() => {
-    setShow(false)
-  }, 5000 )
+  // setTimeout(() => {
+  //   setShow(false)
+  // }, 5000 )
+
+  // setInterval(() => {
+  //   setTimer(timer+1)
+  // }, 1000)
 
   return (
     <div>
       <Layout>
-       {show? <BurgerBuilder /> :  null}
+        <BurgerBuilder />
       </Layout>
     </div>
   );
